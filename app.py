150b75b5eb6a9cc46dde511cf91d4807e38a83d8
jadenbh13 <68014752+jadenbh13@users.noreply.github.com>
2021-06-24 17:35:11 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template
+import os
+app = Flask(__name__)
+
+file1 = "dir.txt"
+file2 = "coordX.txt"
+file3 = "coordY.txt"
+@app.route('/')
+def audio():
+    return render_template('saw.html')
+
+di = ""
+@app.route('/<mn>/giveVal')
+def giveVal(mn):
+    with open(file1, 'w') as filetowrite:
+        filetowrite.write(mn)
+    return mn
+
+@app.route('/<n>/coordX')
+def poseX(n):
+    with open(file2, 'w') as filetowrite:
+        filetowrite.write(n)
+    return n
+@app.route('/<n>/coordY')
+def poseY(n):
+    with open(file3, 'w') as filetowrite:
+        filetowrite.write(n)
+    return n
+
+@app.route('/<x>/<y>/coords')
+def coords(x, y):
+    with open(file2, 'w') as filetowrite:
+        filetowrite.write(x)
+    with open(file3, 'w') as filetowrite:
+        filetowrite.write(y)
+    return x
+
+
+@app.route('/getVal')
+def getVal():
+    f = open("dir.txt", "r")
+    return f.read()
+
+
+@app.route('/getCoord')
+def getCoord():
+    f = open(file2, "r")
+    l = open(file3, "r")
+    return l.read()
---
+++ crazyTest.py
@@ -0,0 +1,142 @@
+import time
+
+import cflib.crtp
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.log import LogConfig
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.crazyflie.syncLogger import SyncLogger
+from cflib.utils import uri_helper
+
+# URI to the Crazyflie to connect to
+uri = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7E7')
+
+# Change the sequence according to your setup
+#			 x	y	z  YAW
+sequence = [
+	(0, 0, 0.1, 0),
+	(0, 0, 0.2, 0),
+	(0, 0, 0.3, 0)
+]
+fileX = "coordX.txt"
+fileY = "coordY.txt"
+def wait_for_position_estimator(scf):
+	print('Waiting for estimator to find position...')
+
+	log_config = LogConfig(name='Kalman Variance', period_in_ms=500)
+	log_config.add_variable('kalman.varPX', 'float')
+	log_config.add_variable('kalman.varPY', 'float')
+	log_config.add_variable('kalman.varPZ', 'float')
+
+	var_y_history = [1000] * 10
+	var_x_history = [1000] * 10
+	var_z_history = [1000] * 10
+
+	threshold = 0.001
+
+	with SyncLogger(scf, log_config) as logger:
+		for log_entry in logger:
+			data = log_entry[1]
+
+			var_x_history.append(data['kalman.varPX'])
+			var_x_history.pop(0)
+			var_y_history.append(data['kalman.varPY'])
+			var_y_history.pop(0)
+			var_z_history.append(data['kalman.varPZ'])
+			var_z_history.pop(0)
+
+			min_x = min(var_x_history)
+			max_x = max(var_x_history)
+			min_y = min(var_y_history)
+			max_y = max(var_y_history)
+			min_z = min(var_z_history)
+			max_z = max(var_z_history)
+
+			# print("{} {} {}".
+			#	   format(max_x - min_x, max_y - min_y, max_z - min_z))
+
+			if (max_x - min_x) < threshold and (
+					max_y - min_y) < threshold and (
+					max_z - min_z) < threshold:
+				break
+
+
+def reset_estimator(scf):
+	cf = scf.cf
+	cf.param.set_value('kalman.resetEstimation', '1')
+	time.sleep(0.1)
+	cf.param.set_value('kalman.resetEstimation', '0')
+
+	wait_for_position_estimator(cf)
+
+
+def position_callback(timestamp, data, logconf):
+	x = data['kalman.stateX']
+	y = data['kalman.stateY']
+	z = data['kalman.stateZ']
+	print('pos: ({}, {}, {})'.format(x, y, z))
+
+
+def start_position_printing(scf):
+	log_conf = LogConfig(name='Position', period_in_ms=500)
+	log_conf.add_variable('kalman.stateX', 'float')
+	log_conf.add_variable('kalman.stateY', 'float')
+	log_conf.add_variable('kalman.stateZ', 'float')
+
+	scf.cf.log.add_config(log_conf)
+	log_conf.data_received_cb.add_callback(position_callback)
+	log_conf.start()
+
+
+def landDrone(cf):
+	for i in range(30):
+		cf.commander.send_position_setpoint(0, 0, 0.1, 0)
+		time.sleep(0.1)
+	cf.commander.send_stop_setpoint()
+
+def setPose(cf, xCord, yCord):
+	posi = (0, yCord, xCord, 0)
+	cf.commander.send_position_setpoint(0, yCord, xCord, 0)
+
+def run_sequence(scf, sequence):
+	cf = scf.cf
+	try:
+		setPose(cf, 0.6, 0)
+		time.sleep(2)
+		while True:
+			fx = open("coordX.txt", "r")
+			fy = open("coordY.txt", "r")
+			xC = fx.read()
+			yC = fy.read()
+			if xC != None and xC != "":
+				mX = 0.6 + (float(xC) / 5)
+				if mX < 0.05:
+					mX = 0.05
+				#print(mX)
+				if yC != None and yC != "":
+					mY = float(yC) / 5
+					print(mY)
+					setPose(cf, mX, mY)
+
+	except KeyboardInterrupt:
+
+		print("   ")
+		print("   ")
+		print("Deads")
+		print("   ")
+		print("   ")
+		print('Closing!')
+		for i in range(30):
+			cf.commander.send_position_setpoint(0, 0, 0.1, 0)
+			time.sleep(0.1)
+		cf.commander.send_stop_setpoint()
+	# Make sure that the last packet leaves before the link is closed
+	# since the message queue is not flushed before closing
+
+
+if __name__ == '__main__':
+	cflib.crtp.init_drivers()
+
+	with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
+		#reset_estimator(scf)
+		# start_position_printing(scf)
+		run_sequence(scf, sequence)
